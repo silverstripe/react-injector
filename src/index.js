970821a45af78ff5ebd949fc30ebfa518c864d3b
Garion Herman <garion@silverstripe.com>
2019-07-24 09:58:10 +1200

    v0.2.0

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+// Specific polyfills required for older browsers
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/symbol';
+
 // Injector.js is exposed on the global / window object so apps using it can share state
 require('expose-loader?Injector!./Injector');
