8f94e255a4322194bb299abea41f54eeb131b23e
Elliot Sawyer <elliot@cryptopay.nz>
2019-10-10 21:18:08 +1300

    FIX: Prevent eval statements from appearing in production build
    Fixes https://github.com/silverstripe/react-injector/issues/5

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    devtool: 'eval-source-map',
+    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
